8671f499a5a426a244734039ac035df5d45b2bb6
Sam Craig <sammidysam@gmail.com>
2013-04-03 18:53:07 -0400

    Updated GamePatcher

--- res/javascripts/layout_blog.js
+++ res/javascripts/layout_blog.js
@@ -1,10 +1,14 @@
 function displayComments(){
-	var FAILED = 'true';
-	var disqus_shortname = 'sammidysamwebsite';
-	var disqus_url = 'http://sammidysam.github.com{{ page.url }}';
-	(function() {
-		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	})();
+	document.write("<div id="disqus_thread"></div>
+		<script type="text/javascript">
+			var disqus_shortname = 'sammidysamwebsite';
+			var disqus_url = 'http://sammidysam.github.com{{ page.url }}';
+			var disqus_identifier = '0';
+			(function() {
+				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			})();
+		</script>
+		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>");
 }
